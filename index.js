900d747b53d540e7e4750600a2ef2e7645a3d15e
Misaki Michiba <61775829+misakimichiba@users.noreply.github.com>
2025-02-04 07:47:15 +0000

    Completed all tests (File uploaded results in JSON response that includes name, type and size of uploaded file)

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-var express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// /api/fileanalyse
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  // var fileName = req.file.originalname;
+  res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
